4e3992a1c046438f49b272c26bf7db092d61a464
J-Alex <jlex.wd@outlook.com>
2016-03-04 17:50:40 -0600

    Front-End Integrations

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,45 +1,27 @@
 <!DOCTYPE html>
-<html>
+<html lang="en">
     <head>
+        <meta charset="UTF-8">
         <title>Laravel</title>
-
         <link href="https://fonts.googleapis.com/css?family=Lato:100" rel="stylesheet" type="text/css">
-
-        <style>
-            html, body {
-                height: 100%;
-            }
-
-            body {
-                margin: 0;
-                padding: 0;
-                width: 100%;
-                display: table;
-                font-weight: 100;
-                font-family: 'Lato';
-            }
-
-            .container {
-                text-align: center;
-                display: table-cell;
-                vertical-align: middle;
-            }
-
-            .content {
-                text-align: center;
-                display: inline-block;
-            }
-
-            .title {
-                font-size: 96px;
-            }
-        </style>
+        <link rel="stylesheet" href="res/components/bootstrap/dist/css/bootstrap.min.css">
+        <link rel="stylesheet" href="res/css/styles.css">
     </head>
-    <body>
+    <body data-ng-app="moduleName">
+      <main data-ng-controller="controllerName">
         <div class="container">
             <div class="content">
                 <div class="title">Laravel 5</div>
             </div>
         </div>
+        <section data-ng-view=""></section>
+      </main>
+      <script src="res/components/jquery/dist/jquery.min.js"></script>
+      <script src="res/components/angular/angular.min.js"></script>
+      <script src="res/components/angular-route/angular-route.min.js"></script>
+      <script src="res/components/angular-resource/angular-resource.min.js"></script>
+      <!-- <script src="/js/angular.js"></script> -->
+      <script src="res/components/bootstrap/dist/js/bootstrap.min.js"></script>
+      <script src="/res/js/scripts.min.js"></script>
     </body>
 </html>
